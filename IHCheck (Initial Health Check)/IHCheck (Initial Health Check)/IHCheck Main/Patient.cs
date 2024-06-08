e42646f8edf0ab2ea2ea4d360ea3ff037622804a
myuga05 <muhyugabillawalrizqi@gmail.com>
2024-06-08 17:46:43 +0700

    inisiasi attribut patient

---
+++ IHCheck (Initial Health Check)/IHCheck (Initial Health Check)/IHCheck Main/Patient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHCheck__Initial_Health_Check_.IHCheck_Main
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string BloodGroup { get; set; }
+        public double Weight { get; set; }
+        public string MedicalHistory { get; set; }
+        public string CurrentDisease { get; set; }
+        public string Treatment { get; set; }      
+
+        public override string ToString()
+        {
+            return $"ID: {Id}\nName: {Name}\nAge: {Age}\nGender: {Gender}\nBlood Pressure: {BloodGroup}\nWeight: {Weight}\nMedical History: {MedicalHistory}\nCurrent Disease: {CurrentDisease}\nTreatment: {Treatment}";
+        }
+    }
+}
--- IHCheck (Initial Health Check)/IHCheck (Initial Health Check)/Program.cs
+++ IHCheck (Initial Health Check)/IHCheck (Initial Health Check)/Program.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IHCheck__Initial_Health_Check_.IHCheck_Main;
 
 namespace IHCheck__Initial_Health_Check_
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+           
         }
     }
 }
